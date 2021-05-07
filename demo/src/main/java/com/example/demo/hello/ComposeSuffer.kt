231e835fd1a23af8918541a079a661a389086474
zhenglinfeng <404836009@qq.com>
2021-05-07 09:41:41 +0800

    新增首页页面样例Demo

--- demo/src/main/java/com/example/demo/hello/ComposeSuffer.kt
+++ demo/src/main/java/com/example/demo/hello/ComposeSuffer.kt
@@ -1,73 +1,114 @@
 package com.example.demo.hello
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.android.base.config.AppManager
 import com.android.base.utils.openNext
+import com.example.demo.R
+import com.example.demo.banner.ui.BannerPager
 import com.example.demo.dialog.DialogActivity
+import com.example.demo.title.LocationBar
 
+
+@Preview(showBackground = true)
+@Composable
+fun rootSuffer() {
+
+    /**
+     * surface可以定义画布大小代替限定符适配
+     */
+    Surface(modifier = Modifier.width(375.dp)) {
+
+        BottomView()
+
+    }
+}
+
+@Composable
+fun Index() {
+    Column {
+        LocationBar()
+
+    }
+
+}
 
 
 @Composable
-fun rootSuffer(){
+fun AppCenter(modifier: Modifier) {
 
-    Surface(modifier = Modifier.width(375.dp)) {
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Surface(modifier = Modifier
-                .height(55.dp)
-                .width(55.dp)
-                .clickable(onClick = {
-                    AppManager.endOfStack().openNext<DialogActivity>()
-                }),color = Color.Green) {
-                Text(
-                    text = "弹出框样例"
-                )
-            }
-            Surface(modifier = Modifier
-                .height(55.dp)
-                .width(55.dp),color = Color.Green) {
-                Text(
-                    text = "Second item"
-                )
-            }
-            Surface(modifier = Modifier
-                .height(55.dp)
-                .width(55.dp),color = Color.Green) {
-                Text(
-                    text = "Second item"
-                )
-            }
-            Surface(modifier = Modifier
-                .height(55.dp)
-                .width(55.dp),color = Color.Green) {
-                Text(
-                    text = "Second item"
-                )
-            }
-            Surface(modifier = Modifier
-                .height(55.dp)
-                .width(55.dp),color = Color.Green) {
-                Text(
-                    text = "Second item"
-                )
-            }
-            Surface(modifier = Modifier
-                .height(55.dp)
-                .width(55.dp),color = Color.Green) {
-                Text(
-                    text = "Second item"
-                )
-            }
+}
 
+
+@Composable
+fun Hot(modifier: Modifier) {
+
+}
+
+@Composable
+fun MySelf(modifier: Modifier) {
+
+}
+
+
+@Composable
+fun BottomView() {
+    val (selectedTab, setSelectedTab) = remember { mutableStateOf(CourseTabs.HOME_PAGE) }
+    val tabs = CourseTabs.values()
+    Scaffold(
+        backgroundColor = colorResource(id = R.color.bgColor),
+        bottomBar = {
+            BottomNavigation(modifier = Modifier.height(56.dp)) {
+                tabs.forEach { tab ->
+                    BottomNavigationItem(selected = tab == selectedTab,
+                        onClick = { setSelectedTab(tab) },
+                        icon = {
+                            Icon(tab.icon, contentDescription = null)
+                        },
+                        label = {
+                            Text(stringResource(tab.title).toUpperCase())
+                        })
+                }
+
+            }
         }
+    ) { innerPadding ->
+        val modifier = Modifier.padding(innerPadding)
+        when (selectedTab) {
+            CourseTabs.HOME_PAGE -> Index()
+            CourseTabs.PROJECT -> AppCenter(modifier)
+            CourseTabs.OFFICIAL_ACCOUNT -> Hot(modifier)
+            CourseTabs.MINE -> MySelf(modifier)
+        }
+
     }
+
+}
+
+enum class CourseTabs(
+    @StringRes val title: Int,
+    val icon: ImageVector
+) {
+    HOME_PAGE(R.string.home_page, Icons.Filled.Favorite),
+    PROJECT(R.string.project, Icons.Filled.Favorite),
+    OFFICIAL_ACCOUNT(R.string.official_account, Icons.Filled.Favorite),
+    MINE(R.string.mine, Icons.Filled.Favorite)
 }
---
+++ demo/src/main/java/com/example/demo/title/TitleBar.kt
@@ -0,0 +1,131 @@
+package com.example.demo.title
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+
+@Preview(showBackground = true)
+@Composable
+fun NormalBar(icon: ImageVector = Icons.Filled.ArrowBack, title: String = "这个是标题") {
+    TopAppBar(backgroundColor = Color.White) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = rememberVectorPainter(image = icon),
+                contentDescription = "",
+                modifier = Modifier.fillMaxHeight()
+            )
+            Text(text = title, modifier = Modifier.align(Alignment.Center), color = Color.Black)
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LocationBar(icon: ImageVector = Icons.Filled.ArrowDropDown) {
+
+
+    Surface(modifier = Modifier.width(375.dp)) {
+
+        TopAppBar(backgroundColor = Color.White) {
+            ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+                val (locationName, locationIcon, searchBg, msgIcon) = createRefs()
+                Text(
+
+                    text = "温州市鹿城区",
+                    color = Color.Black,
+                    fontSize = 12.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .width(50.dp)
+                        .constrainAs(locationName) {
+                            start.linkTo(parent.start)
+                            top.linkTo(parent.top)
+                            bottom.linkTo(parent.bottom)
+                        }
+                        .padding(start = 5.dp)
+                )
+                Image(
+                    painter = rememberVectorPainter(image = icon),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .constrainAs(locationIcon) {
+                            start.linkTo(locationName.end)
+                            top.linkTo(locationName.top)
+                            bottom.linkTo(locationName.bottom)
+                        }
+                )
+                Surface(
+                    Modifier
+                        .width(260.dp)
+                        .height(30.dp)
+                        .clip(RoundedCornerShape(15.dp))
+                        .constrainAs(searchBg) {
+                            start.linkTo(locationIcon.end)
+                            top.linkTo(parent.top)
+                            bottom.linkTo(parent.bottom)
+                        },
+                    color = Color.LightGray
+                ) {
+
+                    Row(
+                        modifier = Modifier
+                            .wrapContentWidth()
+                            .fillMaxHeight(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = rememberVectorPainter(image = Icons.Filled.Search),
+                            contentDescription = "",
+                        )
+                        Text(
+                            text = "搜索",
+                            color = Color.Gray,
+                            fontSize = 12.sp,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            modifier = Modifier
+                                .wrapContentWidth()
+                                .padding(start = 5.dp)
+                        )
+                    }
+
+                }
+
+                Image(
+                    painter = rememberVectorPainter(image = Icons.Filled.Menu),
+                    contentDescription = null,
+                    modifier = Modifier.constrainAs(msgIcon){
+                        end.linkTo( parent.end)
+                        start.linkTo(searchBg.end)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                    }
+                )
+            }
+        }
+    }
+
+}
